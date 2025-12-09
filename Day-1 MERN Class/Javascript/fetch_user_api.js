3b2396850a0039278c1b68ed856bc3a33445d061
athilgithub <athils.23cse@kongu.edu>
2025-12-09 09:03:48 +0530

    Add user detail page and update fetch UI

--- Day-1 MERN Class/Javascript/fetch_user_api.js
+++ Day-1 MERN Class/Javascript/fetch_user_api.js
@@ -29,12 +29,28 @@ window
       images.style.height = "250px";
       images.style.width = "250px";
       images.style.borderRadius = "50px";
-      sub_container.style.backgroundColor = "gray";
+      sub_container.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
       sub_container.style.margin = "10px";
       sub_container.style.borderRadius = "10px 30px";
       sub_container.style.display="flex"
       sub_container.style.alignItems="center"
       sub_container.style.justifyContent="space-evenly"
+      sub_container.style.color = "white";
+      sub_container.classList.add("user-card");
+      
+      // Make card clickable to view user details on new page
+      sub_container.onclick = () => {
+        const params = new URLSearchParams({
+          id: person.id,
+          login: person.login,
+          type: person.type,
+          avatar: person.avatar_url,
+          nodeId: person.node_id,
+          url: person.html_url
+        });
+        window.location.href = `user_details.html?${params.toString()}`;
+      };
+      
       console.log(sub_container);
     });
   })
